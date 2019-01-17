0a333799578b8ddc876a0c69871edcb8e831bc7d
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-01-17 09:40:17 +0100

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int tal1int1;
+            Console.WriteLine("Skriv tal1:");
+                
+            string tal1 = Console.ReadLine();
+            tal1int1 = Convert.ToInt32(tal1);
+            Console.WriteLine(tal1);
+            Console.ReadLine();
+
+            Console.WriteLine(tal1int1);
+
+            
+        
+            
+           
+      
+        }
+    }
+}
